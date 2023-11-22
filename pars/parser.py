af44bd27a9e795f47e02ce560f588c1e523aab75
GitAlexandr <sashabushuev03@mail.ru>
2023-11-22 12:10:22 +0300

    some_work...

--- pars/parser.py
+++ pars/parser.py
@@ -4,11 +4,6 @@ from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
 import time
 
-import requests
-from bs4 import BeautifulSoup
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
-import time
 
 def parse_data(url):
     retry_attempts = 3
@@ -18,7 +13,7 @@ def parse_data(url):
         try:
             user_agent_rotator = UserAgent(software_names=[SoftwareName.CHROME.value], operating_systems=[OperatingSystem.WINDOWS.value], limit=100)
             headers = {'User-Agent': user_agent_rotator.get_random_user_agent()}
-
+            # proxies= {'142.93.218.24', '142.93.218.24'} 
             response = requests.get(url, headers=headers)
             response.raise_for_status()
 
@@ -34,10 +29,8 @@ def parse_data(url):
 
             print(f"Received 429 error. Retrying attempt {current_attempt + 1} with a different user agent...")
             headers = {'User-Agent': user_agent_rotator.get_random_user_agent()}
-            time.sleep(5 * (current_attempt + 1))  # Increase the delay between retries
+            time.sleep(5 * (current_attempt + 1))
             current_attempt += 1
 
     print(f"All retry attempts failed. Unable to fetch data from {url}.")
     return None
-
-
